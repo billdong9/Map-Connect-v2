060f855ef4112af8a118b49390d94860c03edd41
18510047382 <bill-dong@hotmail.com>
2023-03-26 18:09:14 -0400

    add mas build

--- vue.config.js
+++ vue.config.js
@@ -11,15 +11,38 @@ module.exports = defineConfig({
                 win: {
                     icon: "build/icons/icon.ico"
                 },
+                // MAS DEV
+                // mac: {
+                //     icon: "build/icons/icon.icns",
+                //     target: "mas-dev",
+                //     provisioningProfile: "build/mas/Map_Connect_Development.provisionprofile",
+                //     entitlements: "build/mas/entitlements.mac.plist",
+                //     entitlementsInherit: "build/mas/entitlements.mac.inherit.plist",
+                //     hardenedRuntime: false,
+                //     gatekeeperAssess: false,
+                //     identity: null
+                // },
+                // MAS DIS
                 mac: {
                     icon: "build/icons/icon.icns",
-                    target: [
-                        {
-                            target: "dmg",
-                            arch: "x64"
-                        }
-                    ]
-                }
+                    target: "mas",
+                    provisioningProfile: "build/mas/Map_Connect_Distribution.provisionprofile",
+                    entitlements: "build/mas/entitlements.mac.plist",
+                    entitlementsInherit: "build/mas/entitlements.mac.inherit.plist",
+                    hardenedRuntime: false,
+                    gatekeeperAssess: false,
+                    identity: null
+                },
+                // DARWIN
+                // mac: {
+                //     icon: "build/icons/icon.icns",
+                //     target: [
+                //         {
+                //             target: "dmg",
+                //             arch: "x64"
+                //         }
+                //     ]
+                // }
             }
         }
     }
